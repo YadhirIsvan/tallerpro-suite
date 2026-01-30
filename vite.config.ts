6490a2ead9a6ecd79740fbc1235df6ceba4ec79d
Angel Yamir <innovogen375@gmail.com>
2026-01-30 12:07:54 -0600

    sitemap automatico configurado, actualizacion del index e imagenes para mejor CEO, completo

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+import Sitemap from 'vite-plugin-sitemap'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -12,7 +12,20 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    Sitemap({ 
+      hostname: 'https://autotronia-website.vercel.app',
+      dynamicRoutes: [
+        '/',
+        '/caracteristicas',
+        '/beneficios',
+        '/demo',
+        '/contacto',
+      ]
+    }), 
+    mode === "development" && componentTagger()
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
